eb92bb4b51d31c1bc57c4339dbe6d3f1670a781d
jitesh <jiteshprm@gmail.com>
2020-06-14 16:32:47 +0100

    Add sudo in kpartx in image maker Add priviledged instruction option in docker compose in order to mount img device Update test commands in run linux

--- run_linux.sh
+++ run_linux.sh
@@ -13,14 +13,34 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 mv linux.env .env
 dd if=/dev/zero of=/tmp/loop bs=1M count=100
-losetup /dev/loop0 /tmp/loop
+losetup /dev/loop100 /tmp/loop
 
-pvcreate /dev/loop0
-vgcreate vg1 /dev/loop0
-lvcreate --size 90M --name lv1 vg1
-mkfs.xfs /dev/vg1/lv1
+sudo pvcreate /dev/loop100
+sudo vgcreate vg1 /dev/loop100
+sudo lvcreate --size 90M --name lv1 vg1
+sudo mkfs.xfs /dev/vg1/lv1
+
+#sudo apt install lvm2
+#sudo apt install xfsprogs
 
 #docker run \
 #  --rm -it \
+#  -v /home/birdofprey-nvidia/hdd/docker/data:/home/dev/data \
 #  --mount='type=volume,dst=/opt,volume-driver=local,volume-opt=type=xfs,volume-opt=device=/dev/vg1/lv1' \
-#  jiteshprm/nxp-riotboard-image-builder:lastest
+#  jiteshprm/nxp-riotboard-image-builder:latest ls
+
+
+#docker run \
+#--rm -it \
+#-v /home/birdofprey-nvidia/hdd/docker/data:/home/dev/data \
+#--mount='type=volume,dst=/opt,volume-driver=local,volume-opt=type=xfs,volume-opt=device=/dev/vg1/lv1' \
+#jiteshprm/nxp-riotboard-image-builder:latest \
+#"ls /home/dev"
+
+docker run \
+--rm -it \
+--privileged \
+-v /home/birdofprey-nvidia/hdd/docker/data:/home/dev/data \
+jiteshprm/nxp-riotboard-image-builder:latest \
+"cd /home/dev/data;file ubuntu-20.04-preinstalled-server-armhf+raspi.img;mkdir /home/dev/m;sudo mount -v -o offset=269484032 -t ext4 ubuntu-20.04-preinstalled-server-armhf+raspi.img /home/dev/m; ls -lart /home/dev/m; df -h"
+
