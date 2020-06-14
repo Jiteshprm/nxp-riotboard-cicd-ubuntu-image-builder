5ae1773800c2ef7360735ea613517f4e7c587023
jitesh <jiteshprm@gmail.com>
2020-06-14 19:36:41 +0100

    Add mechanism to delete bad images not that were not well constructed

--- scripts/run_riotboard_ubuntu_image_maker.sh
+++ scripts/run_riotboard_ubuntu_image_maker.sh
@@ -42,6 +42,9 @@ echo "We are at path: $(pwd)"
 
 cd ${UBUNTU_IMAGE_DIR}
 
+#List BAD constructed images from folder
+ls -lart "*-BAD.img"
+
 #Remove BAD constructed images from folder
 rm -rf "*-BAD.img"
 
